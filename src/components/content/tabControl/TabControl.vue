d226d269f4508008dafdf05790b48a4dab0a2fbf
xinaiguo <30284532+xinaiguo@users.noreply.github.com>
2021-01-14 19:33:46 +0800

    add scroll and backup

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -29,7 +29,8 @@ export default {
   },
   methods:{
       itemClick(index){
-          this.currentIndex = index
+          this.currentIndex = index;
+          this.$emit('tabClick',index);
       }
   }
 };
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,45 +1,18 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>6</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <scroll class="content" ref="scroll">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control
+        class="tab-control"
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+      ></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>  
+    <back-top @click.native="backClick"></back-top>
   </div>
 </template>
 
@@ -50,6 +23,9 @@ import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView.vue";
 import FeatureView from "./childComps/FeatureView.vue";
 import TabControl from "../../components/content/tabControl/TabControl.vue";
+import GoodsList from "../../components/content/goods/GoodsList.vue";
+import Scroll from "../../components/common/scroll/Scroll.vue";
+import BackTop from '../../components/content/backTop/BackTop.vue';
 
 export default {
   name: "Home",
@@ -59,6 +35,9 @@ export default {
     RecommendView,
     FeatureView,
     TabControl,
+    GoodsList,
+    Scroll,
+    BackTop,
   },
 
   data() {
@@ -70,15 +49,39 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] },
       },
+      currentType: "pop",
     };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
   },
   created() {
     this.getHomeMultidata();
-    this.getHomeGoods('pop');
-    this.getHomeGoods('new');
-    this.getHomeGoods('sell');
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+
+    backClick(){
+      this.$refs.scroll.scrollTo(0,0,500);
+    },
+
     getHomeMultidata() {
       getHomeMultidata().then((res) => {
         console.log(res);
@@ -87,12 +90,13 @@ export default {
       });
     },
 
+    //请求商品数据
     getHomeGoods(type) {
-      const page = this.goods[type].page + 1
+      const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then((res) => {
-        this.goods[type].list.push(...res.data.list);
-        // this.goods[type].page ++;
-        console.log(res.data);
+        this.goods[type].list.push(...res.data.data.list); //将数值保存在list中
+        this.goods[type].page += 1; //page加1
+        console.log(res.data.data);
       });
     },
   },
@@ -120,5 +124,14 @@ export default {
   position: sticky;
   top: 44px;
   z-index: 9;
+}
+
+.content{
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
 }
 </style>
