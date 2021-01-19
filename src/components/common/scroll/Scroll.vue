5c8228a84e21939dd2350bb37b082e4b3314fd8e
Xinai <xinai.guo@siemens.com>
2021-01-19 17:31:48 +0800

    home

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -7,30 +7,59 @@
 </template>
 
 <script>
-import BScroll from 'better-scroll'
+import BScroll from "better-scroll";
 
 export default {
   name: "Scroll",
-  data(){
-      return{
-          scroll:null
-      }
+  props: {
+    probeType: {
+      type: Number,
+      default: 0,
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false,
+    },
   },
-  mounted(){
-      this.scroll = new BScroll(this.$refs.wrapper,{
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+    });
 
-      })
-  },
-  methods:{
-    scrollTo(x,y,time=300){
-      this.scroll.scrollTo(x,y,time);
+    // 监听滚动位置
+    this.scroll.on("scroll", (position) => {
+      this.$emit("scroll", position);
+    });
+
+    //监听上拉加载更多
+    if (this.pullUpLoad) {
+      this.scroll.on("pullingUp", () => {
+        this.$emit("pullingUp");
+      });
     }
-  }
+  },
+  methods: {
+    scrollTo(x, y, time = 300) {
+      this.scroll && this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp() {
+      this.scroll && this.scroll.finishPullUp();
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh();
+    },
+  },
 };
 </script>
 
 <style scoped>
-.wrapper{
-    
+.wrapper {
 }
 </style>
